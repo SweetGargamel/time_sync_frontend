ccd37ab56ad4f55373649f5aced676f7cf97ee6f
SweetGargamel <295006525@qq.com>
2025-05-16 16:18:32 +0800

    修改bug

--- src/stores/files.js
+++ src/stores/files.js
@@ -6,7 +6,7 @@ export const useFilesStore = defineStore('files', () => {
   const fileList = ref([]) // { id: string, name: string, raw: File, status: 'ready' | 'uploading' | 'success' | 'error', progress: number, response: any, uid: string }
 
   const upload_user_file = ref([]) // { id: string, name: string, raw: File, status: 'ready' | 'uploading' | 'success' | 'error', progress: number, response: any, uid: string }
-  const addFile = (file, fileList) => {
+  const addFile = (file) => {
     const newFile = {
       id: uuidv4(),
       uid: uuidv4(), // el-upload uses uid for its internal list management
@@ -20,7 +20,7 @@ export const useFilesStore = defineStore('files', () => {
     return newFile
   }
 
-  const updateFileStatus = (uid, status, fileList, progress = null, response = null) => {
+  const updateFileStatus = (uid, status, progress = null, response = null) => {
     const fileToUpdate = fileList.value.find((f) => f.uid === uid)
     if (fileToUpdate) {
       fileToUpdate.status = status
@@ -33,11 +33,11 @@ export const useFilesStore = defineStore('files', () => {
     }
   }
 
-  const removeFileByUid = (uid, fileList) => {
+  const removeFileByUid = (uid) => {
     fileList.value = fileList.value.filter((f) => f.uid !== uid)
   }
 
-  const getFileByUid = (uid, fileList) => {
+  const getFileByUid = (uid) => {
     return fileList.value.find((f) => f.uid === uid)
   }
 
